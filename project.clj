65d4e458c711accb5aed64395a936bef7c6d3616
Rob Zinkov <rob@zinkov.com>
2012-12-01 05:33:26 -0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject robot "0.2.1-SNAPSHOT"
+  :description "a magical creation powered by a few unorthodox parts"
+  :url "http://github.com/zaxtax/robot"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+		 [org.clojars.zaxtax/java-aws-mturk "1.2.2"]
+		 [clojure-opennlp "0.2.0"]
+		 [log4j/log4j "1.2.16"]
+		 [hiccup "1.0.2"]
+		 ])
---
+++ src/robot/core.clj
@@ -0,0 +1,73 @@
+(ns robot.core
+  (:import (com.amazonaws.mturk.requester HIT)
+	   (com.amazonaws.mturk.service.axis RequesterService)
+	   (com.amazonaws.mturk.util PropertiesClientConfig)
+	   (com.amazonaws.mturk.dataschema QuestionFormAnswers)
+	   (com.amazonaws.mturk.dataschema QuestionFormAnswersType)
+	   (com.amazonaws.mturk.requester Assignment)
+	   (com.amazonaws.mturk.requester AssignmentStatus)
+	   (org.apache.commons.beanutils BeanUtils))
+  (:use [hiccup.core])
+  (:require [clojure.java.io]))
+
+(defn load-props
+  [file-name]
+  (with-open [^java.io.Reader reader (clojure.java.io/reader file-name)] 
+    (let [props (java.util.Properties.)]
+      (.load props reader)
+      (into {} (for [[k v] props] [(read-string k) (read-string v)])))))
+
+(defn has-enough-funds? [service])
+
+(defn create-hit [service title desc price question n-answers]
+  (let [hit (.createHIT service
+			title
+			desc
+			price
+			question
+			n-answers)]
+    hit))
+
+(defn retrieve-all-hits [service]
+  (into [] (.searchAllHITs service)))
+
+(defn clear-hits [service]
+  (map (fn [x] (.disableHIT service (.getHITId x)))
+       (retrieve-all-hits service)))
+
+(defn approve-assignment [service assign-id]
+  (.approveAssignment service assign-id "Good job!"))
+  
+(defn retrieve-submissions [service hit-id]
+  (for [assign (into [] (.getAllAssignmentsForHIT service hit-id))]
+    (if (= (AssignmentStatus/Submitted)
+	   (.getAssignmentStatus assign))
+      (do (approve-assignment service (.getAssignmentId assign))
+	  (.getAnswer assign)))))
+
+(defn start-service []
+  (let [client-config (new PropertiesClientConfig "resources/mturk.properties")
+	service (new RequesterService client-config)]
+	service))
+
+(defn list-question [question choices]
+  (let [prefix "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+                <QuestionForm xmlns=\"http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionForm.xsd\">"
+	suffix "</QuestionForm>"]
+    (str prefix
+	 (html [:Question
+	   [:QuestionIdentifier 1]
+	   [:QuestionContent [:Text question]]
+	   [:AnswerSpecification
+	    [:SelectionAnswer
+	     [:MinSelectionCount 1]
+	     [:MaxSelectionCount 1]
+	     [:StyleSuggestion "radiobutton"]
+	     [:Selections
+	      (for [x choices]
+		[:Selection
+		 [:SelectionIdentifier x]
+		 [:Text x]])]]]])
+	 suffix)))
+
+	      
